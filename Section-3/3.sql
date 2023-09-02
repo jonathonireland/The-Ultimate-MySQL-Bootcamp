73f934d4083979e5f05b4e36111eb580901c9bb1
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2023-09-02 01:29:52 -0400

    Initial Commit

---
+++ Section-3/3.sql
@@ -0,0 +1,78 @@
+-- Section3.17
+SHOW DATABASES;
+CREATE DATABASE chicken_coop;
+SHOW DATABASES;
+DROP DATABASE chicken_coop;
+-- CREATE DATABASE <database_name>;
+CREATE DATABASE soap_store;
+
+-- Section 3.18
+The general command for creating a database:
+
+CREATE DATABASE <database_name>;
+
+A specific example:
+
+CREATE DATABASE soap_store;
+
+
+
+-- Section 3.20
+To drop a database:
+
+DROP DATABASE <database-name>;
+
+To use a database:
+
+USE <database-name>;
+
+-- Section 3.26
+Creating Tables:
+
+CREATE TABLE cats (
+    name VARCHAR(50),
+    age INT
+);
+ 
+CREATE TABLE dogs (
+    name VARCHAR(50),
+    breed VARCHAR(50),
+    age INT
+);
+
+-- Section 3.28
+SHOW tables;
+
+SHOW COLUMNS FROM cats;
+
+DESC cats;
+
+-- Section 3.30
+-- To drop a table:
+
+DROP TABLE <table-name>;
+
+-- To specifically drop the cats table:
+
+DROP TABLE cats;
+
+-- Section 3.32
+-- Create the table:
+
+CREATE TABLE pastries
+  (
+    name VARCHAR(50),
+    quantity INT
+  );
+-- View tables:
+
+SHOW TABLES;
+
+-- View details of pastries table:
+
+DESC pastries;
+
+-- Delete the whole pastries table:
+
+DROP TABLE pastries;
+
